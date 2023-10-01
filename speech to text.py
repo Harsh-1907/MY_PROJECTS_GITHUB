1bb5d4a404e1b4cd97a2286c4bbcf4ec2f85eeb3
Harsh-1907 <146052837+Harsh-1907@users.noreply.github.com>
2023-10-01 21:11:15 +0530

    Add files via upload

---
+++ speech to text.py
@@ -0,0 +1,8 @@
+import speech_recognition as s 
+#create a object of Recognizer
+sr=s.Recognizer()
+print("i am kanye and listening to you...")
+with s.Microphone() as m:
+ audio=sr.listen(m)
+ query=sr.recognize_google(audio,language='eng-in')
+ print(query)
